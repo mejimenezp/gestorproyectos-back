ed2622e6d9c5b10b9c7e8e803fd56d9cb2fc0a5c
mejimenezp <143832231+mejimenezp@users.noreply.github.com>
2024-09-01 13:52:06 -0500

    drop  memory_profiler to requirements

--- Api/views/company_view.py
+++ Api/views/company_view.py
@@ -3,27 +3,26 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 from Api.serializers import CompanySerializer
 from Api.models import Company
-from memory_profiler import profile
+
 
 class CompanyViewSet(viewsets.ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
 
-    @profile
-    @action(detail=True, methods=['get'])
+   
     def projects(self, request, pk=None):
         company = self.get_object()
         projects = company.projects.all()
         return Response({'projects': [project.name for project in projects]})
 
-    @profile
+ 
     def perform_create(self, serializer):
         serializer.save()
 
-    @profile
+   
     def perform_update(self, serializer):
         serializer.save()
 
-    @profile
+
     def perform_destroy(self, instance):
         instance.delete()
